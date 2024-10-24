dcef45c744125e53b60b0e2451a4f0f059ac47b9
cyber-chalk <luka.chalker@gmail.com>
2024-10-24 12:12:26 +1100

    stuff

--- lib.c
+++ lib.c
@@ -1,4 +1,4 @@
-#include "./mnist2.h" // may want to write my own because why not
+#include "./mnist2.h"
 #include <assert.h>
 #include <math.h>
 #include <stdio.h>
@@ -7,7 +7,7 @@
 #define relu(x) ((x) > 0 ? (x) : 0) // maybe make it a leaky reLU
 #define relu_derivative(x) (x > 0) ? 1 : 0
 #define BATCH_SIZE 64 // change if not running on chromebook
-#define NUMLAYERS 3
+#define NUMLAYERS 3 // not including input layer
 #define MOMENTUM 0.9
 // try to use stack allocation wherever possible
 // https://medium.com/@chenymj23/memory-whether-to-store-on-the-heap-or-the-stack-4ff33b2c1e5f
@@ -18,7 +18,7 @@
 double expo(double y) {
   if (y > 80)
     y = 80; // limit prevents overflow in exp
-  return exp(y);
+  return expo(y);
 }
 
 double Softmax(double x, double *Niz, int Iter) {
@@ -26,7 +26,7 @@ double Softmax(double x, double *Niz, int Iter) {
 
   // sum of exponentials
   for (int i = 0; i < Iter; i++) {
-    Sum += expo(Niz[i]);
+    Sum += exp(Niz[i]);
   }
 
   if (Sum == 0)
@@ -60,7 +60,7 @@ void initLayer(Layer *layer, Layer *prev, int size) {
   // (double)rand() / (((double)(RAND_MAX) + 1) / 2); // [0, 2)
   // https://www.baeldung.com/cs/ml-neural-network-weights
   // https://medium.com/@shauryagoel/kaiming-he-initialization-a8d9ed0b5899
-  double stddev = sqrt(2.0 / prevNum); // stdev
+  double stddev = sqrt((double) (2.0 / prevNum)); // stdev
   for (int i = 0; i < n; i++) {
     layer->weights[i] = ((double)rand() / RAND_MAX) * stddev -
                         (stddev / 2); // uniform distribution
@@ -89,19 +89,19 @@ void back(Layer *layer, double *input, double *dInput, double *dOutput,
   int inputSize = (layer->prevLayer == NULL) ? 784 : layer->prevLayer->nnodes;
   if (dInput) {
     for (int j = 0; j < inputSize; j++) {
-      dInput[j] = 0.0f; // maybe calloc
+      // dInput[j] = 0.0f; // calloced
       for (int i = 0; i < layer->nnodes; i++) {
         dInput[j] += dOutput[i] * layer->weights[j * layer->nnodes + i];
       }
     }
   }
 
-  // update weights and momentum foreach input-output connection
+  // update weights and momentum foreach input/output connection
   for (int j = 0; j < inputSize; j++) {
-    double in_j = input[j]; // Input node value
+    double in_j = input[j];
     for (int i = 0; i < layer->nnodes; i++) {
       double grad = dOutput[i] *
-                    in_j; // Gradient for weight between input[j] and output[i]
+                    in_j; // gradient for weight between input[j] and output[i]
 
       // weight momentum (momentum * prev momentum + learning rate *
       // current gradient)
@@ -109,7 +109,7 @@ void back(Layer *layer, double *input, double *dInput, double *dOutput,
           MOMENTUM * layer->weightM[j * layer->nnodes + i] +
           learningRate * grad;
 
-      // Update weights using momentum
+      // update weights thru momentum
       layer->weights[j * layer->nnodes + i] -=
           layer->weightM[j * layer->nnodes + i];
     }
@@ -119,32 +119,61 @@ void back(Layer *layer, double *input, double *dInput, double *dOutput,
   for (int i = 0; i < layer->nnodes; i++) {
     // update bias momentum
     layer->biasM[i] = MOMENTUM * layer->biasM[i] + learningRate * dOutput[i];
-
     // update bias thru momentum
     layer->biases[i] -= layer->biasM[i];
   }
 }
 
+void train(Network *net, double* image, double learningRate) {
+  double *output = calloc(net->hidden[0].nnodes, sizeof(double));
+  double *output2 = calloc(net->hidden[1].nnodes, sizeof(double));
+  double *output3 = calloc(net->hidden[2].nnodes, sizeof(double));
+  double *finalOutput = calloc(net->hidden[2].nnodes, sizeof(double));
+  // 4
+  forward(&net->hidden[0], image, output); // first layer
+  forward(&net->hidden[1], output, output2);
+  forward(&net->hidden[2], output2, output3);
+   forward(&net->hidden[3], output3, finalOutput);
+
+//  int numLayers;
+//     double **outputs = malloc(NUMLAYERS + 1) * sizeof(double*));
+
+//     for (int i = 0; i < numLayers; i++) {
+//         outputs[i] = calloc(net->hidden[i].nnodes, sizeof(double));
+//     }
+
+//     forward(&net->hidden[0], image, outputs[0]); // First layer
+//     for (int i = 1; i < numLayers; i++) {
+//         forward(&net->hidden[i], outputs[i - 1], outputs[i]);
+//     }
+
+//     // Use outputs[numLayers - 1] as the final output if needed
+
+//     // Free allocated memory
+//     for (int i = 0; i < numLayers; i++) {
+//         free(outputs[i]);
+//     }
+//     free(outputs);
+
+}
+
 int main() {
   srand(time(NULL));
-  int batchSize = 10;             // Define the size of the batch
-  double images[batchSize][SIZE]; // To store the batch of images
-  int labels[batchSize];          // To store the batch of labels
+  int batchSize = 10;
+  double images[batchSize][SIZE]; 
+  int labels[batchSize];
 
-  int seekto = 0; // Start from the first image in the dataset
-  int test = 0;   // Load training data (test=0)
+  int seekto = 0;
+  int test = 0; 
 
-  // Load a batch of MNIST data
   load_mnist(test, seekto, batchSize, images, labels);
   seekto += batchSize;
 
-  // Print the batch of images and their corresponding labels
   printf("Printing batch of images:\n");
-  print_mnist_pixel(images, batchSize); // Print the images
-  print_mnist_label(labels, batchSize); // Print the corresponding labels
-
-  // maybe put into function?
+  print_mnist_pixel(images, batchSize);
+  print_mnist_label(labels, batchSize);
   Network net;
+  // // Layer layers[NUMLAYERS];
 
   initLayer(&net.hidden[0], NULL, 16); // first layer
   for (int i = 1; i < NUMLAYERS; i++)
